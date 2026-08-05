00d3f90b3b0ee58ddbbdb3500283fc7546ae9f48
shyaurx <shifaauranuradiansyah@gmail.com>
2026-08-05 13:23:08 +0700

    Add files via upload

---
+++ Video/Windows Form/Admin Forms/Form1/Form2Register.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form2Register : Form
+    {
+        public Form2Register()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Berhasil Daftar, Silahkan Login");
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            Form1 loginForm = new Form1();
+            Close();
+            loginForm.Show();
+        }
+    }
+}
