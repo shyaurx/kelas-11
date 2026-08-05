2252ea5734a26e86a6de08726bbd311c33e3b805
shyaurx <shifaauranuradiansyah@gmail.com>
2026-08-05 13:05:46 +0700

    Add files via upload

---
+++ Video/Windows Form/Admin Forms/Form1/Form3Dashboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form3Dashboard : Form
+    {
+        public Form3Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Form1 loginForm = new Form1();
+            this.Close();
+            loginForm.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Form1 loginForm = new Form1();
+            this.Close();
+            loginForm.Show();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form3Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
