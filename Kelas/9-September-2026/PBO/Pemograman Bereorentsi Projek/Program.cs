d2a651e23bb5e3c2883ead3384bad2bd0e5c8a99
shyaurx <shifaauranuradiansyah@gmail.com>
2026-09-09 13:57:30 +0700

    Add files via upload

---
+++ Kelas/9-September-2026/PBO/Pemograman Bereorentsi Projek/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace Matematika
+{
+    class Home
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Belajar PBO");
+
+            Kalkulator kal = new Kalkulator();
+            //BangunDatar bdatar = new BangunDatar();
+            BangunRuang bruang = new BangunRuang();
+
+            kal.penjumlahan();
+            BangunDatar.luasLingkaran();
+            bruang.volumeTabung();
+        }
+    }
+
+    class Kalkulator
+    {
+        public void penjumlahan()
+        {
+            Console.WriteLine("Penjumlahan");
+        }
+
+        public void pengurangan()
+        {
+            Console.WriteLine("Pengurangan");
+        }
+
+        public void perkalian()
+        {
+            Console.WriteLine("Perkalian");
+        }
+
+        public void Pembagian()
+        {
+            Console.WriteLine("Pembagian");
+        }
+    }
+
+    class BangunDatar
+    {
+        public static void kelilingPersegiPanjang()
+        {
+            Console.WriteLine("Keliling Persegi Panjang");
+        }
+
+        public static void kelililngLingkaran()
+        {
+            Console.WriteLine("Keliling Lingkaran");
+        }
+
+        public static void luasPersegiPanjang()
+        {
+            Console.WriteLine("Luas Persegi Panjang");
+        }
+        
+        public static void luasLingkaran()
+        {
+            Console.WriteLine("Luas Lingkaran");
+        }
+    }
+
+    class BangunRuang
+    {
+        public void volumeBalok()
+        {
+            Console.WriteLine("Volume Balok");
+        }
+
+        public void volumeTabung()
+        {
+            Console.WriteLine("Volume Tabung");
+        }
+
+        public void volumePiramida()
+        {
+            Console.WriteLine("Volume Piramida");
+        }
+    }
+}
